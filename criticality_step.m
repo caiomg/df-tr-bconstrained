6d6374a851e7837f43716c7cdf0b3d0fcb79a1d2
Caio <caio@giuliani.eng.br>
2019-01-18 16:49:13 -0200

    CLEANUP: criticality step
    No change in behavior

--- criticality_step.m
+++ criticality_step.m
@@ -1,4 +1,4 @@
-function model = criticality_step(model, funcs, bl, bu, options)
+function [model, measure] = criticality_step(model, funcs, bl, bu, options)
 % CRITICALITY_STEP -- ensures model is sufficiently poised and with
 % a radius comparable to the gradient
 %
@@ -14,27 +14,20 @@ tol_f = options.tol_f;
 
 initial_radius = model.radius;
 while ~is_lambda_poised(model, options) || is_old(model, options)
-    [model, model_changed] = ensure_improvement(model, funcs, bl, bu, options);
+    model = ensure_improvement(model, funcs, bl, bu, options);
     model.modeling_polynomials = compute_polynomial_models(model);
-    if ~model_changed
-        warning('cmg:model_didnt_change', 'Model didnt change');
-        break
-    end
 end
-while (model.radius > mu*measure_criticality(model, bl, bu))
+measure = norm(measure_criticality(model, bl, bu));
+while (model.radius > mu*measure)
     model.radius = omega*model.radius;
     while ~is_lambda_poised(model, options) || is_old(model, options)
-        [model, model_changed] = ensure_improvement(model, funcs, bl, bu, options);
+        model = ensure_improvement(model, funcs, bl, bu, options);
         model.modeling_polynomials = compute_polynomial_models(model);
-        if ~model_changed
-            warning('cmg:model_didnt_change', 'Model didnt change');
-            break
-        end
     end
-    
-    if (model.radius < tol_radius || ...
-        (beta*norm(measure_criticality(model, bl, bu)) < tol_f) && ...
-        model.radius < 100*tol_radius)
+
+    measure = norm(measure_criticality(model, bl, bu));
+    if (model.radius < tol_radius || (beta*measure < tol_f) && ...
+            model.radius < 100*tol_radius)
         % Better break.
         % Not the end of this algorithm, but satisfies stopping
         % condition for outer algorithm anyway...
@@ -43,7 +36,6 @@ while (model.radius > mu*measure_criticality(model, bl, bu))
 end
 
 % The final radius is increased not to make the reduction drastic
-model.radius = min(max(model.radius, beta*norm(measure_criticality(model, bl, bu))), ...
-                   initial_radius);
+model.radius = min(max(model.radius, beta*measure), initial_radius);
                        
 end

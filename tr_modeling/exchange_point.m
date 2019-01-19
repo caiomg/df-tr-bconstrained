85f4f872fc9074e81db006062c23d24a8940509c
Caio <caio@giuliani.eng.br>
2019-01-19 10:03:41 -0200

    CLENAUP: exchange_point
    Calling exchange_point when the model has fewer than 2 points trhows
    error, instead of warning. This condition is checked at the begining.
    
    Removed one level of indentation.
    All tests pass the same way

--- tr_modeling/exchange_point.m
+++ tr_modeling/exchange_point.m
@@ -1,4 +1,5 @@
-function [model, succeeded, pt_i] = exchange_point(model, new_point, new_fvalues, relative_pivot_threshold)
+function [model, succeeded, pt_i] = ...
+        exchange_point(model, new_point, new_fvalues, relative_pivot_threshold)
 
     pivot_threshold = min(1, model.radius)*relative_pivot_threshold;
 
@@ -6,62 +7,69 @@ function [model, succeeded, pt_i] = exchange_point(model, new_point, new_fvalues
     pivot_polynomials = model.pivot_polynomials;
     center_i = model.tr_center;
     
+    if last_p < 2
+        error('cmg:runtime', 'Error here');
+    end
     
-    if last_p >= 2
-        shift_center = model.points_abs(:, 1);
-        new_point_shifted = new_point - shift_center;
-        points_shifted = model.points_shifted;
-        if last_p <= dim + 1
-            block_beginning = 2;
-            block_end = min(dim+1, last_p);
-        else
-            block_beginning = dim+2;
-            block_end = last_p;
-        end
-        max_val = 0;
-        max_poly_i = 0;
-        for poly_i = block_end:-1:block_beginning
-            if poly_i ~= center_i
-                val = model.pivot_values(poly_i)*...
-                    evaluate_polynomial(pivot_polynomials(poly_i), new_point_shifted);
-                if abs(max_val) < abs(val)
-                    max_val = val;
-                    max_poly_i = poly_i;
-                end
+    shift_center = model.points_abs(:, 1);
+    new_point_shifted = new_point - shift_center;
+    points_shifted = model.points_shifted;
+    if last_p <= dim + 1
+        block_beginning = 2;
+        block_end = min(dim+1, last_p);
+    else
+        block_beginning = dim+2;
+        block_end = last_p;
+    end
+    max_val = 0;
+    max_poly_i = 0;
+    for poly_i = block_end:-1:block_beginning
+        if poly_i ~= center_i
+            val = model.pivot_values(poly_i)*...
+                  evaluate_polynomial(pivot_polynomials(poly_i), new_point_shifted);
+            if abs(max_val) < abs(val)
+                max_val = val;
+                max_poly_i = poly_i;
             end
         end
-        new_pivot_val = max_val;
-        if abs(new_pivot_val) > pivot_threshold
-            points_shifted(:, max_poly_i) = new_point_shifted;
-            % Normalize polynomial value
-            pivot_polynomials(max_poly_i) = normalize_polynomial(pivot_polynomials(max_poly_i), new_point_shifted);
-            % Re-orthogonalize
-            pivot_polynomials(max_poly_i) = orthogonalize_to_other_polynomials(pivot_polynomials, max_poly_i, points_shifted, last_p);
-            
-            % Orthogonalize polynomials on present block (until end)
-            pivot_polynomials = orthogonalize_block(pivot_polynomials, new_point_shifted, max_poly_i, block_beginning, last_p);
-            
-            % Update model
-            cache_size = size(model.cached_points, 2);
-            cache_size = min(cache_size, model.cache_max - 1);
-            model.cached_points = [model.points_abs(:, max_poly_i), model.cached_points(:, 1:cache_size)];
-            model.cached_fvalues = [model.fvalues(:, max_poly_i), model.cached_fvalues(:, 1:cache_size)];
-            model.points_abs(:, max_poly_i) = new_point;
-            model.fvalues(:, max_poly_i) = new_fvalues;
-            model.points_shifted = points_shifted;
-            model.pivot_polynomials = pivot_polynomials;
-            model.pivot_values(:, max_poly_i) = new_pivot_val;
-            model.modeling_polynomials = {};
-            succeeded = true;
-            pt_i = max_poly_i;
-        else
-            succeeded = false;
-            pt_i = 0;
-        end
+    end
+    new_pivot_val = max_val;
+    if abs(new_pivot_val) > pivot_threshold
+        points_shifted(:, max_poly_i) = new_point_shifted;
+        % Normalize polynomial value
+        pivot_polynomials(max_poly_i) = ...
+            normalize_polynomial(pivot_polynomials(max_poly_i), ...
+                                 new_point_shifted);
+        % Re-orthogonalize
+        pivot_polynomials(max_poly_i) = ...
+            orthogonalize_to_other_polynomials(pivot_polynomials, ...
+                                               max_poly_i, points_shifted, ...
+                                               last_p);
+
+        % Orthogonalize polynomials on present block (until end)
+        pivot_polynomials = orthogonalize_block(pivot_polynomials, ...
+                                                new_point_shifted, ...
+                                                max_poly_i, ...
+                                                block_beginning, last_p);
+
+        % Update model
+        cache_size = size(model.cached_points, 2);
+        cache_size = min(cache_size, model.cache_max - 1);
+        model.cached_points = [model.points_abs(:, max_poly_i), ...
+                               model.cached_points(:, 1:cache_size)];
+        model.cached_fvalues = [model.fvalues(:, max_poly_i), ...
+                                model.cached_fvalues(:, 1:cache_size)];
+        model.points_abs(:, max_poly_i) = new_point;
+        model.fvalues(:, max_poly_i) = new_fvalues;
+        model.points_shifted = points_shifted;
+        model.pivot_polynomials = pivot_polynomials;
+        model.pivot_values(:, max_poly_i) = new_pivot_val;
+        model.modeling_polynomials = {};
+        succeeded = true;
+        pt_i = max_poly_i;
     else
         succeeded = false;
         pt_i = 0;
-        warning('cmg:runtime', 'Error here');
     end
 
-    
+end

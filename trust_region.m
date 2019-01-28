f0237ad066b6de7d9f93f63d45e184215e005c2a
Caio <caio@giuliani.eng.br>
2019-01-28 16:59:29 -0200

    FIXED: loop evaluating initial f values, main alg.

--- trust_region.m
+++ trust_region.m
@@ -84,7 +84,7 @@ if n_initial_points == 1
     n_initial_points = 2;
 end
 % Calculating function values for other points of the set
-if length(initial_fvalues) < n_initial_points
+if size(initial_fvalues, 2) < n_initial_points
     for k = 1:n_initial_points
         [initial_fvalues(:, k), succeeded] = evaluate_new_fvalues(funcs, initial_points(:, k));
         if ~succeeded
